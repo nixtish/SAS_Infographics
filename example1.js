e4e8d596edadd09c1ab5e981eacf03bda5175fdc
Sharan <sgopala6@ncsu.edu>
2015-05-04 18:10:32 -0400

    with tooltip wizard

--- example1.js
+++ example1.js
@@ -1,5 +1,7 @@
 $(function() {
 
+// Open File Dialog Box
+
 	$( "#Dialog1" ).dialog({
 	autoOpen: false,
 	modal: true,
@@ -24,6 +26,8 @@ $(function() {
 		$(this).dialog("close");
 	}
 
+//Upload Image Dialog Box
+
 	$( "#Dialog2" ).dialog({
 	autoOpen: false,
 	modal: true,
@@ -40,83 +44,140 @@ $(function() {
 	});
 
 	function upload() {
-		//if($(pic).is(":checked"))
-		$("#drag").append($("input[name=img]:checked").val());
-		$(this).dialog("close");
+		if($(pic).is(":checked"))
+		$("#drag").append($("input[name=img]:checked").src());
 	}
 
-//	("#img1").click(function() {
-//		("#drag").append('<img src="Images/images.png" style="height: 100px; width: 100px;>');
-//	});
+//Draggable components
 		
 	$("#chart").draggable();
 	$("#drag").draggable();
 
-	//Background properties
+//Background properties
 
 	$("#canvas1").click(function() {
+		//$("#imageprop").css("visibility", "hidden");
 		$("#canvasprop").css("visibility", "visible");
 		//$("#imageprop").toggle();
 	});
+
 	$("#white").on("click", function(){
 		$("#canvas1").css("background-color", "white");
 	});
 	$("#blue").on("click", function(){
-		$("#canvas1").css("background-color", "blue");
+		$("#canvas1").css("background-color", "#CCE6FF");
 	});
 	$("#green").on("click", function(){
-		$("#canvas1").css("background-color", "green");
+		$("#canvas1").css("background-color", "#70DB70");
 	});
 	$("#grey").on("click", function(){
-		$("#canvas1").css("background-color", "grey");
+		$("#canvas1").css("background-color", "lightgrey");
+	});
+	$("#red").on("click", function(){
+		$("#canvas1").css("background-color", "#E60000");
 	});
 
-	//Image properties
-	$("#image").click(function() {
-		$("#canvasprop").toggle();
+//Image properties
+
+	/*$("#image").click(function() {
+		//$("#canvasprop").toggle();
+		//$("#canvasprop").css("visibility", "");
 		$("#imageprop").css("visibility", "visible");
 	});
+	*/
 
-	//Textbox
+//Textbox 
 
 	var counter = 1;
-
+	var rem=0;
 	$("#text").click(function(){
 		var newDiv = $(document.createElement('div')).attr("id", 'textbox' + counter);
-		newDiv.after().html('<input type="text" name="text' + counter + 
-	      '" id="text' + counter + '"/>');
+		newDiv.after().html('<textarea name="text' + counter + 
+	      '" id="text' + counter + '"></textarea>');
 		newDiv.appendTo("#canvas1");
-		counter++;
 
 		$("#textbox"+counter).draggable();
-		$("#textbox2").draggable();
-		$("#textbox3").draggable();
-		$("#textbox4").draggable();
-		$("#textbox5").draggable();
-		$("#textbox6").draggable();
-		$("#textbox7").draggable();
-		$("#textbox8").draggable();
+		counter++;
+		rem++;
+		$("#textprop").css("visibility", "visible");
 	});
 	
 	$("#textbutton").click(function() {
 		if(counter>0)
 		{
-			$("#textbox"+counter).remove();
 			counter--;
+			$("#textbox"+counter).remove();
+			//counter--;
+			rem--;
 		}
-		$("#textprop").css("visibility", "hidden");
+		if(rem<1)
+		{
+			$("#textprop").css("visibility", "hidden");
+		}
+		
+
 	});
 
+	/*$("#textbox"+counter).on("click", "#text"+counter, function() {
+		$("#textprop").css("visibility", "visible");
+	});*/
 
-	$("#textbox").click(function() {
+	$("#textbox1").click(function(){
 		$("#textprop").css("visibility", "visible");
 	});
 
-	
+//Tooltips Dialog Boxes
 
-	//Tooltip wizard
+	$("#wizard").click(function(){
+		$("#Dialog3").dialog("open");
+	});
 
-
+	$( "#Dialog3" ).dialog({
+	autoOpen: false,
+	modal: true,
+	buttons:{
+		Cancel: function() {
+			$(this).dialog("close");
+		},
+        Next: function(){
+            $(this).dialog("close");
+            $("#TooltipTypeDialog").dialog("open");
+        }
+	},
+	}); 
+    
+    $( "#TooltipTypeDialog" ).dialog({
+	autoOpen: false,
+	modal: true,
+	buttons:{
+		Cancel: function() {
+			$(this).dialog("close");
+		},
+        Previous: function(){
+            $(this).dialog("close");
+            $("#TooltipsDialog").dialog("open");
+        },
+        Next: function(){
+            $(this).dialog("close");
+            $("#TooltipProperties").dialog("open");
+        }
+	},
+	}); 
+		
+    $( "#TooltipProperties" ).dialog({
+	autoOpen: false,
+	modal: true,
+	buttons:{
+		Cancel: function() {
+			$(this).dialog("close");
+		},
+        Previous: function(){
+            $(this).dialog("close");
+            $("#TooltipTypeDialog").dialog("open");
+        },
+        //Next: addData,
+        }
+	}); 
 	
 });
 
@@ -133,3 +194,7 @@ $(function() {
 	$("#OW").button().on("click", function(){
 			$("p").html("Changed data");
 		});*/
+
+//	("#img1").click(function() {
+//		("#drag").append('<img src="Images/images.png" style="height: 100px; width: 100px;>');
+//	});
